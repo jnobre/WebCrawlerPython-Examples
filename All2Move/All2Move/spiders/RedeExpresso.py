b6aed16481d69e32d8644f9a46df28b53888ec05
João Nobre <joaoanobre@gmail.com>
2015-12-28 16:17:53 +0000

    All2move scrapy

--- All2Move/All2Move/spiders/RedeExpresso.py
+++ All2Move/All2Move/spiders/RedeExpresso.py
@@ -4,10 +4,11 @@ import scrapy
 
 class RedeexpressoSpider(scrapy.Spider):
     name = "RedeExpresso"
-    allowed_domains = ["http://www.rede-expressos.pt/"]
+    allowed_domains = ["rede-expressos.pt"]
     start_urls = (
-        'http://www.http://www.rede-expressos.pt//',
+        'http://www.rede-expressos.pt/',
     )
 
     def parse(self, response):
-        pass
+        filename = response.url.split("/")[-2]
+        open(filename, 'wb').write(response.body)
