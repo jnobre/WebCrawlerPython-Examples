10f044342304ec44ec71d3f2b8a38bb03cbd440b
João Nobre <joaoanobre@gmail.com>
2016-01-22 17:57:04 +0000

    item..

--- All2Move/All2Move/spiders/RedeExpresso.py
+++ All2Move/All2Move/spiders/RedeExpresso.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy.spider 	 import BaseSpider
+from scrapy.selector import Selector
 
 class RedeexpressoSpider(scrapy.Spider):
     name = "RedeExpresso"
@@ -10,5 +11,14 @@ class RedeexpressoSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
-        open(filename, 'wb').write(response.body)
+    #    filename = response.url.split("/")[-2]
+     #   open(filename, 'wb').write(response.body)
+     	print response
+        sel = Selector(response)
+        sites = sel.xpath('//a')
+        for site in sites:
+            title = site.xpath('a/text()').extract()
+            link = site.xpath('@href').extract()
+            desc = site.xpath('text()').extract()
+            print title, link, desc
+		#return items
